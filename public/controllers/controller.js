3f901f367a71f2e39c78a6e732787eeab4d459a3
Hector DeJesus <hdejesus@mergingstreams.com>
2016-11-01 10:39:30 -0700

    Create backup for this file before removing the data.

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,41 +1,31 @@
 var myApp = angular.module('myApp', []);
+
 myApp.controller('AppCtrl', ['$scope', '$http',
     function($scope, $http) {
         console.log("Hello World from controller");
 
-        $http.get('/contactlist');
+        var refresh = function () {
 
-        person1 = {
-            name: 'Tim',
-            email: 'tim@example.com',
-            phone: '(810) 555-3443'
-        };
-        person2 = {
-            name: 'Jonas',
-            email: 'jonas@example.com',
-            phone: '(252) 555-8743'
-        };
-        person3 = {
-            name: 'Rick',
-            email: 'gorick@example.com',
-            phone: '(310) 555-6789'
-        };
-        person4 = {
-            name: 'Fabian',
-            email: 'fabguy@example.com',
-            phone: '(513) 555-2385'
-        };
-        person5 = {
-            name: 'Luc',
-            email: 'lucteam@example.com',
-            phone: '(619) 555-6550'
+            $http.get('/contactlist').success(function (response) {
+                console.log("I got the data I requested");
+                $scope.contactlist = response;
+                $scope.contact = "";
+            });
+
         };
 
-        var contactlist = [person1, person2, person3, person4, person5];
-        $scope.contactlist = contactlist;
+        refresh();
+
+        $scope.addContact = function () {
+            console.log($scope.contact);
+            $http.post('/contactlist', $scope.contact).success(function (response) {
+                console.log('response');
+                refresh();
+            });
+
+        };
+
     }
-
-
 
 ]);
 
--- server.js
+++ server.js
@@ -6,46 +6,33 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 /** app.get('/', function (req, res) {
     res.send("Hello world from server.js")
 });
 */
 
-
 app.use (express.static( __dirname + "/public"));
+app.use (bodyParser.json());
+
+
 app.get('/contactlist', function (req, res) {
     console.log("I received a GET request");
 
-    person1 = {
-        name: 'Tim',
-        email: 'tim@example.com',
-        phone: '(810) 555-3443'
-    };
-    person2 = {
-        name: 'Jonas',
-        email: 'jonas@example.com',
-        phone: '(252) 555-8743'
-    };
-    person3 = {
-        name: 'Rick',
-        email: 'gorick@example.com',
-        phone: '(310) 555-6789'
-    };
-    person4 = {
-        name: 'Fabian',
-        email: 'fabguy@example.com',
-        phone: '(513) 555-2385'
-    };
-    person5 = {
-        name: 'Luc',
-        email: 'lucteam@example.com',
-        phone: '(619) 555-6550'
-    };
+    db.contactlist.find(function (err, docs)
+    {
+        console.log(docs);
+        res.json(docs);
+    });
 
-    var contactlist = [person1, person2, person3, person4, person5];
-    res.json(contactlist);
+});
 
+app.post('/contactlist', function (req, res) {
+    console.log(req.body);
+    db.contactlist.insert(req.body, function(err, doc) {
+        res.json(doc);
+    });
 });
 
 app.listen(3000);
